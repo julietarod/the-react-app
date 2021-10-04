b34ea5d4e9f3260b17db31899d9e39959127d2db
julietarod <julietarodriguezba@gmail.com>
2021-10-04 20:22:38 +0200

    created component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1> Weather App</h1>
+        <Weather />
       
 
       <footer>This project was developed by <a href="https://github.com/julietarod" target="_blank" rel="noreferrer">Julieta Rod </a> and is {" "}
