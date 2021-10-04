b694c411299cb6f4d0498d12c9a0d6080d93861c
julietarod <julietarodriguezba@gmail.com>
2021-10-04 16:34:11 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
+      <div className="container">
+        <h1> Weather App</h1>
+      
 
       <footer>This project was developed by <a href="https://github.com/julietarod" target="_blank" rel="noreferrer">Julieta Rod </a> and is {" "}
         <a href="https://github.com/julietarod/the-react-app" target="_blank" rel="noreferrer">
            open sourced on GitHub
       </a>
-      </footer>
+      </footer> 
+      </div>
+      
     </div>
   );
 }
