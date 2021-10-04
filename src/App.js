3ed7bb6ce45f00736949a6d964757d7edc6c4f6d
julietarod <julietarodriguezba@gmail.com>
2021-10-04 14:19:20 +0200

    added author and links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1> Weather App</h1>
+
+      <footer>This project was developed by <a href="https://github.com/julietarod" target="_blank" rel="noreferrer">Julieta Rod </a> and is {" "}
+        <a href="https://github.com/julietarod/the-react-app" target="_blank" rel="noreferrer">
+           open sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
