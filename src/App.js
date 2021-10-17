635a9aa54bdd3d3964af7a82d03ab592d4c7cdb6
julietarod <julietarodriguezba@gmail.com>
2021-10-17 15:39:21 +0200

    search engine working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 import "./Weather.css";
-
-
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <small> The Weather App</small>
-        
-        <Weather defaultCity="Madrid" /> 
-      
 
-      <footer>This project was developed by <a href="https://github.com/julietarod" target="_blank" rel="noreferrer">Julieta Rod </a> and is {" "}
-        <a href="https://github.com/julietarod/the-react-app" target="_blank" rel="noreferrer">
-           open sourced on GitHub
-      </a>
-      </footer> 
+        <Weather defaultCity="Madrid" />
+
+        <footer>
+          This project was developed by{" "}
+          <a
+            href="https://github.com/julietarod"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julieta Rod{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/julietarod/the-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
       </div>
-      
     </div>
   );
 }
