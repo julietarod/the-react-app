c6a52d3b8514533c4987198bf2e9c18092b145fe
julietarod <julietarodriguezba@gmail.com>
2021-10-20 13:19:49 +0200

    added icon component

--- src/FixedDate.js
+++ src/FixedDate.js
@@ -13,8 +13,14 @@ export default function FixedDate(props) {
 
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
-  let minutes = props.date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return (
     <div>
       {day} {hours}:{minutes}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FixedDate from "./FixedDate.js";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,17 +14,16 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt={props.data.description} />
-          <span className="temperature">
+          <WeatherIcon code={props.data.icon} />
+          <div className="temperature">
             {Math.round(props.data.temperature)}
-          </span>
+          </div>
           <span className="unit"> °C </span>
         </div>
         <div className="col-6">
           <ul>
             <li>Precipitation: 85%</li>
             <li>Humidity: {props.data.humidity}</li>
-            <li>Wind: {props.data.wind}</li>
           </ul>
         </div>
       </div>
