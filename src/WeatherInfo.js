f5c28ed5466e242776c675466db397eedbca8476
julietarod <julietarodriguezba@gmail.com>
2021-10-21 13:20:04 +0200

    added celsius /fahrenheit component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FixedDate from "./FixedDate.js";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature.js";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,15 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
-          <div className="temperature">
-            {Math.round(props.data.temperature)}
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+
+              <div className="temperature">
+                <WeatherTemperature celsius={props.data.temperature} />
+              </div>
+            </div>
           </div>
-          <span className="unit"> °C </span>
         </div>
         <div className="col-6">
           <ul>
